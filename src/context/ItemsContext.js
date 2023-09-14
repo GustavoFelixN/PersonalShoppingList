4cac27709241787d347be598078a0e60a4f3a5c4
Gustavo Felix <gfnascimento@inf.ufsm.br>
2023-09-14 20:03:40 -0300

    Adding reducer on items context data

--- src/context/ItemsContext.js
+++ src/context/ItemsContext.js
@@ -1,28 +1,50 @@
-import { createContext, useCallback, useState } from 'react'; 
+import { createContext, useCallback, useReducer } from 'react';
 
 export const ItemsContext = createContext();
 
-export const ItemsContextProvider = ({children}) => {
-	const [loading, setLoading] = useState(false);
-	const [items, setItems] = useState([]);
-	const [error, setError] = useState('');
+const initialState = {
+	items: [],
+	loading: false,
+	error: '',
+};
 
+const reducer = (state, action) => {
+	switch(action.type) {
+		case 'GET_ITEMS_SUCCESS':
+			return {
+				...state,
+				items: action.payload,
+				loading: false,
+			};
+		case 'GET_ITEMS_ERROR':
+			return {
+				...state,
+				items: [],
+				loading: false,
+				error: action.payload,
+			};
+		default:
+			return state;
+	}
+};
+
+export const ItemsContextProvider = ({children}) => {
+	const [state, dispatch] = useReducer(reducer, initialState);
 	const fetchItems = useCallback(async (listId) => {
 		try {
 			const data = await fetch(`https://my-json-server.typicode.com/GustavoFelixN/PersonalShoppingList/lists/${listId}/items`);
 			const results = await data.json();
 			if (results) {
-				setItems(results);
-				setLoading(false);
+
+				dispatch({type: 'GET_ITEMS_SUCCESS', payload: results});
 			}
 		} catch(e) {
-			setLoading(false);
-			setError(e.message);
+			dispatch({type: 'GET_ITEMS_ERROR', payload: e.message});
 		}
 	}, []);
 
 	return (
-		<ItemsContext.Provider value={{items, error, loading, fetchItems}}>
+		<ItemsContext.Provider value={{...state, fetchItems}}>
 			{children}
 		</ItemsContext.Provider>
 	);
