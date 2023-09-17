e63c823360255f519d5a569fa034cd547fcc76d2
Gustavo Felix <gfnascimento@inf.ufsm.br>
2023-09-16 23:42:52 -0300

    Lazyly loading pages

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
-import React from 'react';
+import React, { Suspense, lazy } from 'react';
 import styled, { createGlobalStyle } from 'styled-components';
 import { Route, Routes, BrowserRouter } from 'react-router-dom';
 import Header from './components/Header/Header';
-import Lists from './pages/Lists';
-import ListDetail from './pages/ListDetail';
-import ListForm from './pages/ListForm';
 import AppContext from './context/AppContext';
+
+
+const Lists = lazy(() => import('./pages/Lists'));
+const ListDetail = lazy(() => import('./pages/ListDetail'));
+const ListForm = lazy(() => import('./pages/ListForm'));
 
 const GlobalStyle = createGlobalStyle`
   body {
@@ -29,14 +31,16 @@ function App() {
 			<GlobalStyle />
 			<AppWrapper>
 				<BrowserRouter>
-					<AppContext>
-						<Header />
-						<Routes>
-							<Route path='/' element={<Lists />} />
-							<Route path='/list/:listId/new' element={<ListForm />} />
-							<Route path='/list/:listId' element={<ListDetail />} />
-						</Routes>
-					</AppContext>
+					<Header />
+					<Suspense fallback={<div>Loading...</div>}>
+						<AppContext>
+							<Routes>
+								<Route path='/' element={<Lists />} />
+								<Route path='/list/:listId/new' element={<ListForm />} />
+								<Route path='/list/:listId' element={<ListDetail />} />
+							</Routes>
+						</AppContext>
+					</Suspense>
 				</BrowserRouter>
 			</AppWrapper>
 		</>
